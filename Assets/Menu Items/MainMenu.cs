0b4485d093a165776abe9cf7272084ae2eb93048
Grant Weigel <grantweigel@hotmail.com>
2023-03-29 01:20:17 -0500

    Minor Fixes

--- Assets/Menu Items/MainMenu.cs
+++ Assets/Menu Items/MainMenu.cs
@@ -12,6 +12,11 @@ public class MainMenu : MonoBehaviour
     
     public void QuitGame()
     {
-        Application.Quit();
+#if UNITY_STANDALONE
+                Application.Quit();
+        #endif
+        #if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+        #endif
     }
 }
